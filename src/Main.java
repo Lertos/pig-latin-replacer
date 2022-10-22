7bfaf45f5360f4e97a35314f659d7b7b82729e5e
Lertos <deetasticgaming@gmail.com>
2022-10-22 16:31:12 -0300

    Getting scanner loop created

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        String response = "";
 
+        System.out.println("Please enter a sentence and press Enter. Pig Latin will be returned.");
+
+        while (!response.equalsIgnoreCase("q")) {
+            response = scanner.nextLine();
+
+            if (response.equalsIgnoreCase("q")) {
+                break;
+            }
+
+            //Show the response - being the converted phrase into pig latin
+            System.out.println("");
+            System.out.println("\nSupply another phrase, or type 'q' to quit.");
+        }
     }
 }
